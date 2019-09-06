bae8efc8930f9b717508e6255a09c8054f2797ec
APM-SYSTEM <APM-SYSTEM@DESKTOP-488FP15>
2019-09-06 08:01:38 +0530

    1st project

---
+++ src/test/java/Test/Demo.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("welcome",true);
+	}
+
+}
